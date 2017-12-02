d1e27ba18c016af4f73f3e6f5dcf5d29856aee15
SlashD9 <colin.p.s.daly@gmail.com>
2017-12-02 20:41:58 +0000

    Generated some components to test Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BandComponent } from './components/band/band.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    BandComponent
   ],
   imports: [
     BrowserModule
