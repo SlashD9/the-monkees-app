6215af9bb24934cf4eed06277439f01731d6d80f
SlashD9 <colin.p.s.daly@gmail.com>
2017-12-02 21:05:24 +0000

    Got Router Working with home and band

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BandComponent } from './components/band/band.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'band', component: BandComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { BandComponent } from './components/band/band.component';
     BandComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
